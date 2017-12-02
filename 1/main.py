08624b1ce101e45058daea46568d3d307ae2d76d
austburn <austburn@gmail.com>
2017-12-02 09:13:22 -0600

    treat list as circular

--- 1/main.py
+++ 1/main.py
@@ -4,11 +4,8 @@ import sys
 def captcha(line):
     captcha_sum = 0
     for idx, ch in enumerate(line):
-        if idx+1 < len(line) and line[idx] == line[idx+1]:
+        if line[idx] == line[(idx+1)%len(line)]:
             captcha_sum += int(line[idx])
-
-    if len(line) > 1 and line[0] == line[-1]:
-        captcha_sum += int(line[0])
     return captcha_sum
 
 if __name__ == '__main__':
