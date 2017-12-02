be4ce6ba43e737af83477d076fa19fedf6077749
austburn <austburn@gmail.com>
2017-12-02 08:55:31 -0600

    fixed to solve

--- 2/main.py
+++ 2/main.py
@@ -1,7 +1,10 @@
-def checksum(code):
-    ceiling, floor = int(code[0]), int(code[0])
+import sys
 
-    for char in code:
+
+def line_checksum(line):
+    ceiling, floor = int(line[0]), int(line[0])
+
+    for char in line:
         char_int = int(char)
         if char_int < floor:
             floor = char_int
@@ -9,3 +12,20 @@ def checksum(code):
             ceiling = char_int
 
     return ceiling - floor
+
+
+def file_checksum(lines):
+    file_sum = 0
+    for line in lines:
+        full_line = line.split()
+        file_sum += line_checksum(full_line)
+    return file_sum
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage:')
+        print('\tpython main.py input.txt')
+    else:
+        with open(sys.argv[1]) as f:
+            print(file_checksum(f.readlines()))
--- 2/test_main.py
+++ 2/test_main.py
@@ -1,19 +1,30 @@
 import unittest
-from main import checksum
+from main import line_checksum, file_checksum
 
 
-class TestChecksum(unittest.TestCase):
+class TestLineChecksum(unittest.TestCase):
     def test_easy(self):
-        self.assertEqual(checksum('1234'), 3)
+        self.assertEqual(line_checksum('1234'), 3)
 
     def test_samples(self):
-        self.assertEqual(checksum('5195'), 8)
+        self.assertEqual(line_checksum('5195'), 8)
 
     def test_decr_string(self):
-        self.assertEqual(checksum('753'), 4)
+        self.assertEqual(line_checksum('753'), 4)
 
     def test_incr_string(self):
-        self.assertEqual(checksum('2468'), 6)
+        self.assertEqual(line_checksum('2468'), 6)
+
+
+class TestFileChecksum(unittest.TestCase):
+    def test_easy(self):
+        input = [
+            '1 2 3 4\n',        # 3
+            '20 50 60 10\n',    # 50
+            '11 19 67 567\n',   # 556
+            '5 8 7 2\n'         # 6
+        ]
+        self.assertEqual(file_checksum(input), 615)
 
 
 if __name__ == '__main__':
