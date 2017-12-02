1332254c129f0ad115e1840ab5f84960ed90def1
austburn <austburn@gmail.com>
2017-12-02 08:21:12 -0600

    first attempt, failing test -> 753 outputs -3, not 4

--- 2/main.py
+++ 2/main.py
@@ -1,2 +1,12 @@
-def checksum(hash):
-    return 0
+def checksum(code):
+    min_ceiling = 0
+    max_floor = 9
+
+    for char in code:
+        char_int = int(char)
+        if char_int < max_floor:
+            max_floor = char_int
+        elif char_int > min_ceiling:
+            min_ceiling = char_int
+
+    return min_ceiling - max_floor
--- 2/test_main.py
+++ 2/test_main.py
@@ -6,6 +6,11 @@ class TestChecksum(unittest.TestCase):
     def test_easy(self):
         self.assertEqual(checksum('1234'), 3)
 
+    def test_samples(self):
+        self.assertEqual(checksum('5195'), 8)
+        self.assertEqual(checksum('753'), 4)
+        self.assertEqual(checksum('2468'), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
