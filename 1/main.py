ee3d4877574717f57526ff10a18f8a76c0fb13c8
austburn <austburn@gmail.com>
2017-12-02 09:23:58 -0600

    finish part 2 of puzzle one

--- 1/main.py
+++ 1/main.py
@@ -1,8 +1,16 @@
 import sys
 
+NEXT = 'NEXT'
+HALF = 'HALF'
 
-def captcha(line, jump):
+def captcha(line, jump_behavior=NEXT):
     captcha_sum = 0
+    jump = 0
+    if jump_behavior == NEXT:
+        jump = 1
+    elif jump_behavior == HALF:
+        jump = int(len(line)/2)
+
     for idx, ch in enumerate(line):
         if line[idx] == line[(idx+jump)%len(line)]:
             captcha_sum += int(line[idx])
@@ -15,4 +23,5 @@ if __name__ == '__main__':
     else:
         with open(sys.argv[1]) as f:
             for line in f:
-                print(captcha(line.rstrip('\n'), 1))
+                print('First: %s' % captcha(line.rstrip('\n')))
+                print('Second: %s' % captcha(line.rstrip('\n'), jump_behavior=HALF))
--- 1/test_main.py
+++ 1/test_main.py
@@ -1,13 +1,20 @@
 import unittest
-from main import captcha
+from main import captcha, HALF
 
 
 class TestLineCaptcha(unittest.TestCase):
     def test_samples(self):
-        self.assertEqual(captcha('1122', 1), 3)
-        self.assertEqual(captcha('1111', 1), 4)
-        self.assertEqual(captcha('1234', 1), 0)
-        self.assertEqual(captcha('91212129', 1), 9)
+        self.assertEqual(captcha('1122'), 3)
+        self.assertEqual(captcha('1111'), 4)
+        self.assertEqual(captcha('1234'), 0)
+        self.assertEqual(captcha('91212129'), 9)
+
+    def test_jump(self):
+        self.assertEqual(captcha('1212', jump_behavior=HALF), 6)
+        self.assertEqual(captcha('1221', jump_behavior=HALF), 0)
+        self.assertEqual(captcha('123425', jump_behavior=HALF), 4)
+        self.assertEqual(captcha('123123', jump_behavior=HALF), 12)
+        self.assertEqual(captcha('12131415', jump_behavior=HALF), 4)
 
 
 if __name__ == '__main__':
