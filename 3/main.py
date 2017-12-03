69cd46becdaea1852e72bee2cc1e8e22b996305f
austburn <austburn@gmail.com>
2017-12-03 16:58:11 -0600

    First pass

--- 3/main.py
+++ 3/main.py
@@ -1,3 +1,5 @@
+import math
+
 # 17  16  15  14  13
 # 18   5   4   3  12
 # 19   6   1   2  11
@@ -11,14 +13,40 @@
 #   + 4 (additional corners)
 def determine_layer(num):
     layer_range = [1]
-    current_layer = 1
+    current_layer = 0
     while num not in layer_range:
         nums_per_side = int(len(layer_range)/4) + 1
         nums_in_next_layer = (nums_per_side * 4) + 4
         # add one as stop is not included in range
         layer_range = range(layer_range[-1] + 1, layer_range[-1] + nums_in_next_layer + 1)
         current_layer += 1
-    return current_layer, layer_range
+    return current_layer, list(layer_range)
+
+
+def determine_coordinates(num):
+    x, y = 0, 0
+    layer, rng = determine_layer(num)
+    idx = rng.index(num)
+    nums_per_side = int(len(rng)/4) + 1
+
+    right = rng[0:nums_per_side-1]
+    right.append(rng[-1])
+    top_right_corner_idx = math.floor(nums_per_side/2)
+    top = rng[top_right_corner_idx:top_right_corner_idx+nums_per_side]
+    left = rng[top_right_corner_idx+nums_per_side-1:nums_per_side*2]
+    bottom = rng[top_right_corner_idx+(nums_per_side*2)-2:]
+
+    if num in right:
+        x = layer
+    if num in left:
+        x = -layer
+
+    if num in top:
+        y = layer
+    if num in bottom:
+        y = -layer
+
+    return x, y
 
 
 if __name__ == '__main__':
--- 3/test_main.py
+++ 3/test_main.py
@@ -1,23 +1,33 @@
 import unittest
-from main import determine_layer
+from main import determine_layer, determine_coordinates
 
 
 class TestDetermineLayer(unittest.TestCase):
-    def test_layer_one(self):
-        self.assertEqual(determine_layer(1), (1, [1]))
+    def test_layer_zero(self):
+        self.assertEqual(determine_layer(1), (0, [1]))
 
-    def test_layer_two(self):
-        expected = (2, range(2, 10))
+    def test_layer_one(self):
+        expected = (1, list(range(2, 10)))
         self.assertEqual(determine_layer(2), expected)
         self.assertEqual(determine_layer(5), expected)
         self.assertEqual(determine_layer(9), expected)
 
-    def test_layer_three(self):
-        expected = (3, range(10, 26))
+    def test_layer_two(self):
+        expected = (2, list(range(10, 26)))
         self.assertEqual(determine_layer(10), expected)
         self.assertEqual(determine_layer(17), expected)
         self.assertEqual(determine_layer(25), expected)
 
+
+class TestDetermineCoordinates(unittest.TestCase):
+    def test_origin(self):
+        self.assertEqual(determine_coordinates(1), (0, 0))
+
+    def test_layer_one(self):
+        self.assertEqual(determine_coordinates(2), (1, 0))
+
+    def test_layer_two(self):
+        self.assertEqual(determine_coordinates(10), (2, 0))
 
 if __name__ == '__main__':
     unittest.main()
