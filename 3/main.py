10369ad78ac4bcc84abc51d962b44f7eef76fb5b
austburn <austburn@gmail.com>
2017-12-03 15:52:40 -0600

    first pass at determining layer

--- 3/main.py
+++ 3/main.py
@@ -1,5 +1,23 @@
+# 17  16  15  14  13
+# 18   5   4   3  12
+# 19   6   1   2  11
+# 20   7   8   9  10
+# 21  22  23  24  25
+# layer 1 - 1
+#       2 - 8
+#       3 - 16
+# So... given layer n, n+1 has:
+#   nums/side * 4
+#   + 4 (additional corners)
 def determine_layer(num):
-    return num
+    layer_range = [1]
+    current_layer = 1
+    while num not in layer_range:
+        nums_per_side = int(len(layer_range)/4) + 1
+        nums_in_next_layer = (nums_per_side * 4) + 4
+        layer_range = range(layer_range[-1], nums_in_next_layer)
+        current_layer += 1
+    return current_layer
 
 
 if __name__ == '__main__':
