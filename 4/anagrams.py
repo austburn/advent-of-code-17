3922cee3167a4bc74428172671720a8ebf2e4131
Austin Burnett <austburn@gmail.com>
2017-12-06 14:46:00 -0600

    Day 4, 2nd star

--- 4/anagrams.py
+++ 4/anagrams.py
@@ -1,13 +1,39 @@
 import sys
 
 
+def anagram(a, b):
+    if len(a) != len(b):
+        return False
+
+    matches = 0
+    for letter in a:
+        try:
+            b.index(letter)
+        except ValueError:
+            return False
+        else:
+            matches += 1
+
+    return matches == len(a)
+
+
+def has_anagrams(phrase):
+    for i, p in enumerate(phrase):
+        for x in range(len(phrase)-1, i, -1):
+            if anagram(p, phrase[x]):
+                return True
+    return False
+
+
+
 def valid_passcodes(codes):
     num_valid_passcodes = 0
     for code in codes:
         phrase = code.split()
         phrase_set = set(phrase)
         if len(phrase) == len(phrase_set):
-            num_valid_passcodes += 1
+            if not has_anagrams(phrase):
+                num_valid_passcodes += 1
 
     return num_valid_passcodes
 
