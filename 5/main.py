5d0688eab0ca7ba9b0a13ebbfb610f77d853a310
Austin Burnett <austburn@gmail.com>
2017-12-06 11:22:21 -0600

    two gold stars

--- 5/main.py
+++ 5/main.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 
 def determine_maze_exit(maze):
@@ -17,6 +18,27 @@ def determine_maze_exit(maze):
     return num_steps
 
 
+def determine_maze_exit_alternate(maze):
+    current_index = 0
+    num_steps = 0
+
+    while True:
+        try:
+            steps = maze[current_index]
+        except IndexError:
+            break
+
+        if steps >= 3:
+            maze[current_index] = steps - 1
+        else:
+            maze[current_index] = steps + 1
+
+        current_index = current_index + steps
+        num_steps += 1
+
+    return num_steps
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage:')
@@ -24,4 +46,6 @@ if __name__ == '__main__':
     else:
         with open(sys.argv[1]) as f:
             maze = [int(line.rstrip('\n')) for line in f.readlines()]
+            maze_dupe = copy.copy(maze)
             print(determine_maze_exit(maze))
+            print(determine_maze_exit_alternate(maze_dupe))
--- 5/test_main.py
+++ 5/test_main.py
@@ -1,10 +1,13 @@
 import unittest
-from main import determine_maze_exit
+from main import determine_maze_exit, determine_maze_exit_alternate
 
 
 class TestDetermineMazeExit(unittest.TestCase):
     def test_sample(self):
         self.assertEqual(determine_maze_exit([0, 3, 0, 1, -3]), 5)
+
+    def test_sample_with_alternate(self):
+        self.assertEqual(determine_maze_exit_alternate([0, 3, 0, 1, -3]), 10)
 
 
 if __name__ == '__main__':
