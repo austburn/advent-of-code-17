72be04986ef2d808ebee4e00d44aa9ffb4e37f68
austburn <austburn@gmail.com>
2017-12-09 19:52:29 -0600

    day 8 base

---
+++ 8/main.py
@@ -0,0 +1,15 @@
+import sys
+import re
+
+
+def parse_instruction(instruction):
+    return {}
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage:')
+        print('\tpython main.py input.txt')
+    else:
+        with open(sys.argv[1]) as f:
+            print('')
---
+++ 8/test_main.py
@@ -0,0 +1,21 @@
+import unittest
+from main import parse_instruction
+
+
+class TestMain(unittest.TestCase):
+    def test_parse_instruction(self):
+        self.assertEqual(parse_instruction('b inc 5 if a > 1'), {
+            'register': 'b',
+            'operation': 'inc',
+            'value': 5,
+            'condition': {
+                'register': 'a',
+                'comparison': '>',
+                'value': 1
+            }
+        })
+
+
+if __name__ == '__main__':
+    unittest.main()
+
