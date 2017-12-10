abfe6eb90f44a689a1e8639bf965fcc4b8f24e96
austburn <austburn@gmail.com>
2017-12-09 20:36:39 -0600

    run instruction set and add test based on sample

--- 8/main.py
+++ 8/main.py
@@ -2,8 +2,20 @@ import sys
 import re
 
 
+OPERATIONS = {
+    '>': lambda x, y: x > y,
+    '<': lambda x, y: x < y,
+    '>=': lambda x, y: x >= y,
+    '<=': lambda x, y: x <= y,
+    '==': lambda x, y: x == y,
+    '!=': lambda x, y: x != y,
+    'inc': lambda x, y: x + y,
+    'dec': lambda x, y: x - y
+}
+
+
 def parse_instruction(instruction):
-    rgx = re.compile('(?P<register>\w) (?P<operation>(inc|dec)) (?P<value>\-?\d) if (?P<condition_register>\w) (?P<condition_operator>[<>!=]+) (?P<condition_value>\-?\d)')
+    rgx = re.compile('(?P<register>\w) (?P<operation>(inc|dec)) (?P<value>\-?\d+) if (?P<condition_register>\w) (?P<condition_operator>[<>!=]+) (?P<condition_value>\-?\d+)')
     match_dict = rgx.match(instruction).groupdict()
     return {
         'register': match_dict['register'],
@@ -17,10 +29,24 @@ def parse_instruction(instruction):
     }
 
 
+def run_instruction_set(instructions):
+    registers = {}
+    for instruction in instructions:
+        parsed_instruction = parse_instruction(instruction)
+        dest_reg = parsed_instruction['register']
+        op_reg = parsed_instruction['condition']['register']
+        if dest_reg not in registers:
+            registers[dest_reg] = 0
+        if op_reg not in registers:
+            registers[op_reg] = 0
+        if OPERATIONS[parsed_instruction['condition']['operation']](registers[op_reg], parsed_instruction['condition']['value']):
+            registers[dest_reg] = OPERATIONS[parsed_instruction['operation']](registers[dest_reg], parsed_instruction['value'])
+    return max(registers.items(), key=lambda x: x[1])
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage:')
         print('\tpython main.py input.txt')
     else:
         with open(sys.argv[1]) as f:
-            print('')
+            run_instruction_set([f.rstrip('\n') for f in f.readlines()])
--- 8/test_main.py
+++ 8/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import parse_instruction
+from main import parse_instruction, run_instruction_set
 
 
 class TestMain(unittest.TestCase):
@@ -26,6 +26,27 @@ class TestMain(unittest.TestCase):
                 'value': 1
             }
         })
+
+    def test_parse_instruction_with_complex_comparison(self):
+        self.assertEqual(parse_instruction('b inc -5 if a >= 1'), {
+            'register': 'b',
+            'operation': 'inc',
+            'value': -5,
+            'condition': {
+                'register': 'a',
+                'operation': '>=',
+                'value': 1
+            }
+        })
+
+    def test_run_instruction_set(self):
+        instructions = [
+            'b inc 5 if a > 1',
+            'a inc 1 if b < 5',
+            'c dec -10 if a >= 1',
+            'c inc -20 if c == 10'
+        ]
+        self.assertEqual(run_instruction_set(instructions), ('a', 1))
 
 
 if __name__ == '__main__':
