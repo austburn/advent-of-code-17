6c93b4415be030b02f364aa0b6d9a2a1eb8dbacd
Austin Burnett <austburn@gmail.com>
2017-12-13 12:57:04 -0600

    day 9 pt 2

--- 9/main.py
+++ 9/main.py
@@ -7,14 +7,17 @@ def traverse_groups(input):
     i = 0
     group_sum = 0
     group_val = 0
+    chars = 0
     while i < len(input):
         c = input[i]
         if c == '!':
             i += 2
             continue
 
-        if c == '<':
+        if c == '<' and not garbage:
             garbage = True
+            i += 1
+            continue
         elif c == '>':
             garbage = False
         elif c == '{' and not garbage:
@@ -23,8 +26,11 @@ def traverse_groups(input):
         elif c == '}' and not garbage:
             group_val -= 1
 
+        if garbage:
+            chars += 1
+
         i += 1
-    return group_sum
+    return group_sum, chars
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
--- 9/test_main.py
+++ 9/test_main.py
@@ -4,13 +4,13 @@ from main import traverse_groups
 
 class TestMain(unittest.TestCase):
     def test_traverse_groups(self):
-        self.assertEqual(traverse_groups('{}'), 1)
-        self.assertEqual(traverse_groups('{{{}}}'), 6)
-        self.assertEqual(traverse_groups('{{},{}}'), 5)
-        self.assertEqual(traverse_groups('{{<ab>},{<ab>},{<ab>},{<ab>}}'), 9)
-        self.assertEqual(traverse_groups('{{<!!>},{<!!>},{<!!>},{<!!>}}'), 9)
-        self.assertEqual(traverse_groups('{{{},{},{{}}}}'), 16)
-        self.assertEqual(traverse_groups('{{<a!>},{<a!>},{<a!>},{<ab>}}'), 3)
+        self.assertEqual(traverse_groups('{}'), (1, 0))
+        self.assertEqual(traverse_groups('{{{}}}'), (6, 0))
+        self.assertEqual(traverse_groups('{{},{}}'), (5, 0))
+        self.assertEqual(traverse_groups('{{<ab>},{<ab>},{<ab>},{<ab>}}'), (9, 8))
+        self.assertEqual(traverse_groups('{{<!!>},{<!!>},{<!!>},{<!!>}}'), (9, 0))
+        self.assertEqual(traverse_groups('{{{},{},{{}}}}'), (16, 0))
+        self.assertEqual(traverse_groups('{{<a!>},{<a!>},{<a!>},{<ab>}}'), (3, 17))
 
 if __name__ == '__main__':
     unittest.main()
