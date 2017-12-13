3af692c2480dae8e4261573217de69be92eb8db3
Austin Burnett <austburn@gmail.com>
2017-12-13 12:49:40 -0600

    didn't think of this one correctly the first go-round

--- 9/main.py
+++ 9/main.py
@@ -2,35 +2,29 @@ import sys
 import re
 
 
-def is_group(input):
-    rgx = re.compile('\{.*\}')
-    return bool(rgx.match(input))
-
-def is_trash(input):
-    rgx = re.compile('\<.*\>')
-    return bool(rgx.match(input))
-
-def remove_voids(input):
-    return input.replace('!!', '').replace('!>', '').replace('!<', '')
-
-def _parse_groups_and_trash(input, group_value=1):
-    import pdb;pdb.set_trace()
-    if input == '':
-        return 0
-    if is_trash(input):
-        return 0
-    if is_group(input):
-        innards = input[1:len(input)-1]
-        if is_trash(innards):
-            return group_value
-        return group_value + sum([_parse_groups_and_trash(c, group_value=group_value+1) for c in innards.split(',')])
-
-    return group_value
-
 def traverse_groups(input):
-    sanitized = remove_voids(input)
-    return _parse_groups_and_trash(sanitized)
+    garbage = False
+    i = 0
+    group_sum = 0
+    group_val = 0
+    while i < len(input):
+        c = input[i]
+        if c == '!':
+            i += 2
+            continue
 
+        if c == '<':
+            garbage = True
+        elif c == '>':
+            garbage = False
+        elif c == '{' and not garbage:
+            group_val += 1
+            group_sum += group_val
+        elif c == '}' and not garbage:
+            group_val -= 1
+
+        i += 1
+    return group_sum
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -38,4 +32,4 @@ if __name__ == '__main__':
         print('\tpython main.py input.txt')
     else:
         with open(sys.argv[1]) as f:
-            print()
+            print(traverse_groups(f.readlines()[0].rstrip('\n')))
--- 9/test_main.py
+++ 9/test_main.py
@@ -1,22 +1,16 @@
 import unittest
-from main import is_group, is_trash, traverse_groups
+from main import traverse_groups
 
 
 class TestMain(unittest.TestCase):
-    def test_is_group(self):
-        self.assertTrue(is_group('{}'))
-
-    def test_is_trash(self):
-        self.assertTrue(is_trash('<>'))
-
     def test_traverse_groups(self):
-        # self.assertEqual(traverse_groups('{}'), 1)
-        # self.assertEqual(traverse_groups('{{{}}}'), 6)
-        # self.assertEqual(traverse_groups('{{},{}}'), 5)
-        # self.assertEqual(traverse_groups('{{<ab>},{<ab>},{<ab>},{<ab>}}'), 9)
-        # self.assertEqual(traverse_groups('{{<!!>},{<!!>},{<!!>},{<!!>}}'), 9)
+        self.assertEqual(traverse_groups('{}'), 1)
+        self.assertEqual(traverse_groups('{{{}}}'), 6)
+        self.assertEqual(traverse_groups('{{},{}}'), 5)
+        self.assertEqual(traverse_groups('{{<ab>},{<ab>},{<ab>},{<ab>}}'), 9)
+        self.assertEqual(traverse_groups('{{<!!>},{<!!>},{<!!>},{<!!>}}'), 9)
+        self.assertEqual(traverse_groups('{{{},{},{{}}}}'), 16)
         self.assertEqual(traverse_groups('{{<a!>},{<a!>},{<a!>},{<ab>}}'), 3)
-
 
 if __name__ == '__main__':
     unittest.main()
